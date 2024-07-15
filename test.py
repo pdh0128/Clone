9638aec534c816898798ddf040536ae96dcb51d9
pdh0128 <pdh0128a@gmail.com>
2024-07-15 16:45:01 +0900

    two

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import streamlit as st
+import json
+key_dict = json.loads(st.secrets["textkey"])
 
-
-cred = credentials.Certificate('secret.json')
+cred = credentials.Certificate(key_dict)
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ioting-3a752-default-rtdb.firebaseio.com'
