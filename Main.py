4f259b58cd80484077ee1349fe645b3ed78119d5
pdh0128 <pdh0128a@gmail.com>
2024-07-15 17:20:48 +0900

    six

--- Main.py
+++ Main.py
@@ -5,14 +5,13 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 import json
-key_dict = json.loads(st.secrets["textkey"])
 
-cred = credentials.Certificate(key_dict)
+    # key_dict = json.loads(st.secrets["textkey"])
+    # cred = credentials.Certificate(key_dict)
 
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://ioting-3a752-default-rtdb.firebaseio.com'
-})
-
+    # firebase_admin.initialize_app(cred, {
+    #         'databaseURL': 'https://ioting-3a752-default-rtdb.firebaseio.com'
+    # })
 
 ref = db.reference('/')
 
