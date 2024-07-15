a20b7e6668aaf889f12b98c462007e8137fd61d0
pdh0128 <pdh0128a@gmail.com>
2024-07-15 16:57:06 +0900

    four

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import time
-
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+
+import json
+key_dict = json.loads(st.secrets["textkey"])
+
+cred = credentials.Certificate(key_dict)
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://ioting-3a752-default-rtdb.firebaseio.com'
+})
 
 ref = db.reference('/')
 
